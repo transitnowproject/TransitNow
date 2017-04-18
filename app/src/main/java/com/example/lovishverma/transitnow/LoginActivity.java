abdc1727aa6eb150c857e250385ed812b4025dbe
TransitNow <transitnowproject@gmail.com>
2017-04-18 09:37:26 -0700

    Final Commit

--- app/src/main/java/com/example/lovishverma/transitnow/LoginActivity.java
+++ app/src/main/java/com/example/lovishverma/transitnow/LoginActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private EditText edtEmail, edtPassword;
 
 
     @Override
@@ -16,15 +20,49 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin= (Button) findViewById(R.id.btnLogin);
-       // btnLinkToRegisterScreen= (Button) findViewById(R.id.btnLinkToRegisterScreen);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        // btnLinkToRegisterScreen= (Button) findViewById(R.id.btnLinkToRegisterScreen);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                final String email = edtEmail.getText().toString();
+                if (!isValidEmail(email)) {
+                    edtEmail.setError("Invalid Email");
+                }
+                final String password = edtPassword.getText().toString();
+                if (!isValidPassword(password)) {
+                    edtPassword.setError("Invalid Password");
+                }
+
             }
+
         });
+
+    }
+
+    private boolean isValidEmail(String email) {
+
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+
+
+    }
+
+    private boolean isValidPassword(String password) {
+        if (password != null && password.length() > 6) {
+            return true;
+        }
+        return false;
+
+    }
 
 //        btnLinkToRegisterScreen.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -33,5 +71,5 @@ public class LoginActivity extends AppCompatActivity {
 //                startActivity(intent);
 //            }
 //        });
-    }
 }
+
--- app/src/main/java/com/example/lovishverma/transitnow/RegistrationActivity.java
+++ app/src/main/java/com/example/lovishverma/transitnow/RegistrationActivity.java
@@ -5,9 +5,11 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import static com.example.lovishverma.transitnow.R.id.btnLinkToRegisterScreen;
 
@@ -15,6 +17,7 @@ public class RegistrationActivity extends AppCompatActivity {
 
     private Button btnRegister,btnMale,btnFemale;
     private EditText edtDOB;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -22,9 +25,10 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         btnRegister= (Button) findViewById(R.id.btnRegister);
-        btnMale= (Button) findViewById(R.id.btnMale);
-        btnFemale= (Button) findViewById(R.id.btnFemale);
+//        btnMale= (Button) findViewById(R.id.btnMale);
+//        btnFemale= (Button) findViewById(R.id.btnFemale);
         edtDOB= (EditText) findViewById(R.id.edtDOB);
+        spinner= (Spinner) findViewById(R.id.spinner);
 
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -35,19 +39,26 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
 
-        btnMale.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //setVisible(false);
-                //btnMale.setBackground(getResources().getColor(R.color.input_register_hint));
-            }
-        });
+//        btnMale.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //setVisible(false);
+//                //btnMale.setBackground(getResources().getColor(R.color.input_register_hint));
+//            }
+//        });
         edtDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //DatePicker dp = new DatePicker();
             }
         });
+
+        String [] gender = getResources().getStringArray(R.array.gender);
+        {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.mylayout,gender);
+            spinner.setAdapter(adapter);
+        }
+
 
     }
 }
