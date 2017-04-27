eaae4f08293702c6110da23e101121a3505a1423
TransitNow <transitnowproject@gmail.com>
2017-04-27 12:44:35 -0700

    Admin & driver activity

--- app/src/main/java/com/example/lovishverma/transitnow/StartActivity.java
+++ app/src/main/java/com/example/lovishverma/transitnow/StartActivity.java
@@ -20,7 +20,7 @@ public class StartActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =  new Intent(StartActivity.this,AreYouActivity.class);
+                Intent intent =  new Intent(StartActivity.this,TabActivity.class);
                 startActivity(intent);
 
             }
